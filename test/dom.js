8f0136de51b941016425fedf7830e4f876e6995a
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-25 18:14:15 +0000

    Changed dom.js file in order to use a more recent version of it

--- test/dom.js
+++ test/dom.js
@@ -1,9 +1,11 @@
 /* dom.js */
-var jsdom = require('jsdom').jsdom;
-var exposedProperties = ['window', 'navigator', 'document'];
-
-global.document = jsdom('');
+import jsdom from 'jsdom';
+const {JSDOM} = jsdom;
+const {document} = (new JSDOM('<!doctype html><html><body></body></html>')).window;
+global.document = document;
 global.window = document.defaultView;
+
+var exposedProperties = ['window', 'navigator', 'document'];
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property);
