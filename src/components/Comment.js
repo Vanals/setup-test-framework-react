e37acfb69605a2b50899797e92c8f1ba73b8c834
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-25 17:56:20 +0000

    Write new test for comment component + created Comment component
    An error rise up when run npm test. Necessary an enzyme adapter

---
+++ src/components/Comment.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Comment extends Component {
+  render() {
+    return (
+      <li>
+      <p> Ciao Marco </p>
+      </li>
+    )
+  }
+}
+
+export default Comment;
--- test/Comment.spec.js
+++ test/Comment.spec.js
@@ -1,5 +1,10 @@
-describe('a passing test', () => {
-  it('should pass', () => {
-    expect(true).to.be.true;
+import React from 'react';
+import Comment from '../src/components/Comment';
+
+describe('Comment item', () => {
+  const wrapper = shallow(<Comment />);
+
+  it('should be a list item', () => {
+    expect(wrapper.type()).to.eql('li');
   });
 });
